00815502afc3be2f13aa51fe872fbf13fe9b987d
JorshuaEs <estradajorshua16@gmail.com>
2022-12-10 17:39:33 -0600

    Ajustando tipos de clase

--- Cuenta.js
+++ Cuenta.js
@@ -2,7 +2,8 @@ export class Cuenta {
     #cliente;
     #saldo;
 
-    constructor(cliente, numero, agencia, saldo) {
+    constructor(tipo, cliente, numero, agencia, saldo) {
+        this.tipo = tipo;
         this.numero = numero;
         this.agencia = agencia;
         this.#cliente = cliente;
@@ -10,12 +11,17 @@ export class Cuenta {
     }
 
     depositoEnCuenta(valor) {
+ 
         if (valor > 0)
             this.#saldo += valor;
         return this.#saldo;
     }
 
     retirarDeCuenta(valor) {
+        if(this.tipo == 'Corriente')
+            valor = valor * 1.05;
+        else if(this.tipo == 'Ahorro')
+            valor = valor * 1.02;
         if (valor <= this.#saldo)
             this.#saldo -= valor;
         return this.#saldo;
