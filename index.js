f150aaffb183129fad0e5dcb8a0f8fa08a8ea301
JorshuaEs <estradajorshua16@gmail.com>
2022-12-12 17:23:05 -0600

    Creacion de empleados y reordenamieto

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 /*Importación de clases*/
 import {Cliente} from './Cliente.js'
-import {CuentaCorriente} from './CuentaCorriente.js';
-import { CuentaAhorro} from './CuentaAhorro.js';
-import { Cuenta } from './Cuenta.js';
-import { CuentaNomina } from './CuentaNomina.js';
+import {CuentaCorriente} from './Cuentas/CuentaCorriente.js';
+import { CuentaAhorro} from './Cuentas/CuentaAhorro.js';
+import { CuentaNomina } from './Cuentas/CuentaNomina.js';
+import {Empleado} from './Empleados/Empleado.js';
+import { Gerente } from './Empleados/Gerente.js';
+import { Director } from './Empleados/Director.js';
 
 
 const cliente = new Cliente('Leonardo','13804050','123224');
 const cliente2 = new Cliente('María','16979808','8989');
 
-const cuentaDeLeonardo = new CuentaCorriente(cliente, '1', '001');
-const cuentaDeMaria = new CuentaCorriente(cliente2,'2','002');
-
-const cuentaAhorroLeonardo = new CuentaAhorro(cliente,'9985','001',0);
-console.log(cuentaAhorroLeonardo);
-
-const cuentaNominaLeonardo = new CuentaNomina(cliente,'9854','001',100);
-
-console.log(cuentaNominaLeonardo);
-cuentaNominaLeonardo.depositoEnCuenta(150);
-console.log(cuentaNominaLeonardo.verSaldo());
-
-cuentaNominaLeonardo.retirarDeCuenta(50);
-console.log(cuentaNominaLeonardo.verSaldo());
+const empleado = new Empleado('Juan Perez','1565165165',10000);
+const gerente = new Gerente('Pedro Rivas','545161616',12000);
+const director = new Director('Elena Moreno','3843834',15000);
+console.log(empleado.verBonificacion());
+console.log(gerente.verBonificacion());
+console.log(director.verBonificacion());
